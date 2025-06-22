df4d933923c56dfd407867fa434a573edced0c61
SaiBhavadeesh <saibhavadeesh@gmail.com>
2025-06-22 13:39:04 +0530

    removed models folder from tracking

--- networksecurity/pipeline/training.py
+++ networksecurity/pipeline/training.py
@@ -1,13 +1,13 @@
 import os, sys
 
+from networksecurity.cloud.s3_syncer import S3Sync
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.components.data_validation import DataValidation
 from networksecurity.components.data_transformation import DataTransformation
 from networksecurity.components.model_trainer import ModelTrainer
-
+from networksecurity.constants.training_pipeline import TRAINING_BUCKET_NAME
 from networksecurity.entity.config import (
     TrainingPipelineConfig,
     DataIngestionConfig,
@@ -15,7 +15,6 @@ from networksecurity.entity.config import (
     DataValidationConfig,
     ModelTrainerConfig,
 )
-
 from networksecurity.entity.artifact import (
     DataIngestionArtifact,
     DataValidationArtifact,
@@ -27,6 +26,7 @@ from networksecurity.entity.artifact import (
 class Training:
     def __init__(self) -> None:
         self.training_pipeline_config = TrainingPipelineConfig()
+        self.s3_sync = S3Sync()
 
     def start_data_ingestion(self):
         try:
@@ -88,18 +88,50 @@ class Training:
             model_trainer = ModelTrainer(
                 data_transformation_artifact, self.model_trainer_config
             )
-            model_trainer_artifact = model_trainer.initiate_model_trainer()
+            model_trainer_artifact: ModelTrainerArtifact = (
+                model_trainer.initiate_model_trainer()
+            )
             logging.info(f"Model trainer completed - {model_trainer_artifact}")
             return model_trainer_artifact
+        except Exception as e:
+            raise NetworkSecurityException(e, sys)  # type: ignore
+
+    def sync_artifact_dir_to_s3(self):
+        try:
+            aws_bucket_url = f"s3://{TRAINING_BUCKET_NAME}/artifact/{self.training_pipeline_config.timestamp}"
+            self.s3_sync.sync_folder_to_s3(
+                folder=self.training_pipeline_config.artifact_dir,
+                aws_bucket_url=aws_bucket_url,
+            )
+        except Exception as e:
+            raise NetworkSecurityException(e, sys)  # type: ignore
+
+    def sync_saved_model_dir_to_s3(self):
+        try:
+            aws_bucket_url = f"s3://{TRAINING_BUCKET_NAME}/models/{self.training_pipeline_config.timestamp}"
+            self.s3_sync.sync_folder_to_s3(
+                folder=self.training_pipeline_config.model_dir,
+                aws_bucket_url=aws_bucket_url,
+            )
         except Exception as e:
             raise NetworkSecurityException(e, sys)  # type: ignore
 
     def run_pipeline(self):
         try:
             data_ingestion_artifact = self.start_data_ingestion()
-            data_validation_artifact = self.start_data_validation(data_ingestion_artifact)
-            data_transformation_artifact = self.start_data_transformation(data_validation_artifact)
-            model_trainer_artifact = self.start_model_trainer(data_transformation_artifact)
+            data_validation_artifact = self.start_data_validation(
+                data_ingestion_artifact
+            )
+            data_transformation_artifact = self.start_data_transformation(
+                data_validation_artifact
+            )
+            model_trainer_artifact = self.start_model_trainer(
+                data_transformation_artifact
+            )
+
+            self.sync_artifact_dir_to_s3()
+            self.sync_saved_model_dir_to_s3()
+            
             return model_trainer_artifact
         except Exception as e:
-            raise NetworkSecurityException(e, sys) # type: ignore
+            raise NetworkSecurityException(e, sys)  # type: ignore
